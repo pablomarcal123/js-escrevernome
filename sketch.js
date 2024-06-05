5a7265bc13c077616d4b59cb7cb362f7976a51b9
baiano <00001160702548sp@al.educacao.sp.gov.br>
2024-06-05 14:53:19 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(800, 800);
+    background('white');
+  }
+  
+  function draw(){ stroke('pink');
+    fill('red'); 
+   
+  
+   // console.log(mouseIsPressed)
+  if(mouseIsPressed) {rect(mouseX, mouseY, 25, 35);}
+                 }
+  // console.log(righ)
